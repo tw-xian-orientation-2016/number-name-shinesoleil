9cc5f4c80a4cf757db55717ab50a5cd301f2dfde
shinesoleil <hao_pan@outlook.com>
2016-03-01 17:37:01 +0800

    add convertNumbers

--- number-name/spec/convertNumbers-spec.js
+++ number-name/spec/convertNumbers-spec.js
@@ -1,5 +1,17 @@
 describe('convertNumbers unit test', function() {
   it('convert [1] to ["one"]', function() {
-    expect(convert([1])).toEqual(['one']);
-  })
+    expect(convertNumbers([1])).toEqual(['one']);
+  });
+  it('convert [12] to ["twelve"]', function() {
+    expect(convertNumbers([12])).toEqual(['twelve']);
+  });
+  it('convert [123] to ["one hundred and twenty three"]', function() {
+    expect(convertNumbers([123])).toEqual(['one hundred and twenty three']);
+  });
+  it('convert [12, 345] to ["twelve hundred", "three hundred and fourty five"]', function() {
+    expect(convertNumbers([12, 345])).toEqual(['twelve', 'three hundred and fourty five']);
+  });
+  it('convert [1, 234, 567] to ["one million", "two hundred and thirty four", "five hundred and sixty seven"]', function() {
+    expect(convertNumbers([1, 234, 567])).toEqual(['one', 'two hundred and thirty four', 'five hundred and sixty seven']);
+  });
 });
