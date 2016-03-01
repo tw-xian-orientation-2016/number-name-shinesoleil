cd9a426b6686e13b3a1fca2fe9f14d9f94b45163
shinesoleil <hao_pan@outlook.com>
2016-03-01 17:29:09 +0800

    format files

---
+++ number-name/spec/convertNumbers-spec.js
@@ -0,0 +1,5 @@
+describe('convertNumbers unit test', function() {
+  it('convert [1] to ["one"]', function() {
+    expect(convert([1])).toEqual(['one']);
+  })
+});
--- number-name/spec/groupByDigits-spec.js
+++ number-name/spec/groupByDigits-spec.js
@@ -1,20 +1,20 @@
 describe('groupByDigits unit test', function() {
-    it('convert 12 to [12]', function() {
-        expect(groupByDigits(12)).toEqual([12]);
-    });
-    it('convert 123 to [123]', function() {
-        expect(groupByDigits(123)).toEqual([123]);
-    });
-    it('convert 1234 to [1, 234]', function() {
-        expect(groupByDigits(1234)).toEqual([1, 234]);
-    });
-    it('convert 12345 to [12, 345]', function() {
-        expect(groupByDigits(12345)).toEqual([12, 345]);
-    });
-    it('convert 1234567 to [1, 234, 567]', function() {
-        expect(groupByDigits(1234567)).toEqual([1, 234, 567]);
-    });
-    it('convert 123456789 to [123, 456, 789]', function() {
-        expect(groupByDigits(123456789)).toEqual([123, 456, 789]);
-    });
+  it('convert 12 to [12]', function() {
+    expect(groupByDigits(12)).toEqual([12]);
+  });
+  it('convert 123 to [123]', function() {
+    expect(groupByDigits(123)).toEqual([123]);
+  });
+  it('convert 1234 to [1, 234]', function() {
+    expect(groupByDigits(1234)).toEqual([1, 234]);
+  });
+  it('convert 12345 to [12, 345]', function() {
+    expect(groupByDigits(12345)).toEqual([12, 345]);
+  });
+  it('convert 1234567 to [1, 234, 567]', function() {
+    expect(groupByDigits(1234567)).toEqual([1, 234, 567]);
+  });
+  it('convert 123456789 to [123, 456, 789]', function() {
+    expect(groupByDigits(123456789)).toEqual([123, 456, 789]);
+  });
 });
