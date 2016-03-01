ec4fde1680ada023111102d56433aaa0e37ebc19
shinesoleil <hao_pan@outlook.com>
2016-03-01 23:54:45 +0800

    convertNumbers done

--- number-name/main/main.js
+++ number-name/main/main.js
@@ -10,3 +10,43 @@ function groupByDigits(number) {
 
   return digitsGroups.reverse();
 }
+
+
+function getOnes() {
+  return ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+}
+
+function getTens() {
+  return ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+}
+
+function getThousands() {
+  return ['', 'thousand', 'million'];
+}
+
+function convertDecade(lastTwoDigits, ones, tens) {
+  if(lastTwoDigits<20) {
+    return ones[lastTwoDigits];
+  } else {
+    return tens[parseInt(lastTwoDigits/10)] + ' ' + ones[lastTwoDigits%10];
+  }
+}
+
+function convertHundred(firstDigit, ones) {
+  return ones[firstDigit] === '' ? '': ones[firstDigit] + ' hundred and ';
+}
+
+function convertNumber(numberThreeDigits, ones, tens) {
+  var firstDigit = parseInt(numberThreeDigits/100);
+  var lastTwoDigits = numberThreeDigits%100;
+
+  return convertHundred(firstDigit, ones) + convertDecade(lastTwoDigits, ones, tens);
+}
+
+function convertNumbers(digitsGroups, ones, tens) {
+  return digitsGroups.map(function(digitsGroup) {
+    return convertNumber(digitsGroup, ones, tens);
+  })
+}
