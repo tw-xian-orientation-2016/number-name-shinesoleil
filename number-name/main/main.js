f1c715379cc641446054746a26942166b2ce699f
shinesoleil <hao_pan@outlook.com>
2016-03-01 17:18:55 +0800

    groupByDigits done

---
+++ number-name/main/main.js
@@ -0,0 +1,12 @@
+function groupByDigits(number) {
+
+    var strNumber = number.toString();
+    var digitsGroups = [];
+
+    for(var len=strNumber.length; len>0; len-=3) {
+        digitsGroups.push(parseInt(strNumber.substr(-3)));
+        strNumber = strNumber.substr(0, len-3);
+    }
+
+    return digitsGroups.reverse();
+}
