0152cc0c7beecf77fffd77669c96a08008814a3c
shinesoleil <hao_pan@outlook.com>
2016-03-01 23:55:12 +0800

    fix test case bug

--- number-name/spec/convertNumbers-spec.js
+++ number-name/spec/convertNumbers-spec.js
@@ -1,17 +1,20 @@
 describe('convertNumbers unit test', function() {
+  var ones = getOnes();
+  var tens = getTens();
+
   it('convert [1] to ["one"]', function() {
-    expect(convertNumbers([1])).toEqual(['one']);
+    expect(convertNumbers([1], ones, tens)).toEqual(['one']);
   });
   it('convert [12] to ["twelve"]', function() {
-    expect(convertNumbers([12])).toEqual(['twelve']);
+    expect(convertNumbers([12], ones, tens)).toEqual(['twelve']);
   });
   it('convert [123] to ["one hundred and twenty three"]', function() {
-    expect(convertNumbers([123])).toEqual(['one hundred and twenty three']);
+    expect(convertNumbers([123], ones, tens)).toEqual(['one hundred and twenty three']);
   });
   it('convert [12, 345] to ["twelve hundred", "three hundred and fourty five"]', function() {
-    expect(convertNumbers([12, 345])).toEqual(['twelve', 'three hundred and fourty five']);
+    expect(convertNumbers([12, 345], ones, tens)).toEqual(['twelve', 'three hundred and forty five']);
   });
   it('convert [1, 234, 567] to ["one million", "two hundred and thirty four", "five hundred and sixty seven"]', function() {
-    expect(convertNumbers([1, 234, 567])).toEqual(['one', 'two hundred and thirty four', 'five hundred and sixty seven']);
+    expect(convertNumbers([1, 234, 567], ones, tens)).toEqual(['one', 'two hundred and thirty four', 'five hundred and sixty seven']);
   });
 });
